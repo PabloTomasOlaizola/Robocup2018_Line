eb03ab746d0ac3d5b43d0531118c72c9b9832ffc
PabloTomasOlaizola <pabloolaizola2016@gmail.com>
2018-03-03 14:09:58 -0300

    SUMO COMENTARIO

--- Arduino/testeo_sensoresCNY70/testeo_sensoresCNY70.ino
+++ Arduino/testeo_sensoresCNY70/testeo_sensoresCNY70.ino
@@ -1,34 +1,36 @@
-const int blanco = 200;
+const int blanco =  200;
 const int nsensores = 7;
+//HACEMOS UN ARRAY CON LOS SENSORES QUE LEEN NEGRO
 int pines[nsensores] = {1,2,3,4,5,6,7};
 
-void setup() {  
+void setup() {
   Serial.begin(9600);
-  
+
+  //ACA DECLARAMOS CADA UNO DE LOS PINES QUE ESTAN EN EL ARRAY COMO INPUT
   for(int i = 0; i < nsensores; i++)
       pinMode(pines[i],INPUT);
 }
 
-bool lee_blanco(int pin)
-{
-      if(analogRead(pines[pin]) < blanco) 
+//ACA PONEMOS QUE CUANDO LEA BLANCO NOS TIRE TRUE
+bool lee_blanco(int pin){
+
+      if(analogRead(pines[pin]) < blanco)
          return false;
 
     return true;
 }
-
+//ACA HACEMOS QUE LEA LOS SENSORES QUE TIENE PARA DESPUES IMPRIMIR
 void loop() {
-  for(int i = 0; i < nsensores; i++)
-  {
+  for(int i = 0; i < nsensores; i++){
+
     print("Sensor ");
     print(i);
     print(": ");
+    //ACA SI LEE BLANCO VA A IMPRIMIR "LEE BLANCO" Y SI NO "NO LEE BLANCO"
     if(lee_blanco(i))
       println("Lee blanco");
      else
      println("No lee blanco");
   }
-    
-
-  delay(2000);
+    delay(2000);
 }
